4c722e78b66fd981939a6e9d801b3e41ffed755b
potato <leducanh27773@gmail.com>
2022-08-29 13:52:04 +0700

    update

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.post('/insert', async (req, res) => {
     const name = req.body.txtName
     const price = req.body.txtPrice
     const url = req.body.txtURL;
+    if(name.length === 0){
+        var result = await getAll("Products")
+        res.render('insert',{products:result, nameErr:'Name cannot be empty!'})
+    }
     if (url.length == 0) {
         var result = await getAll("Products")
         res.render('insert', { products: result, picError: 'Please input the picture of the product!' })
